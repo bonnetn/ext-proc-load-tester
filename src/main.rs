5a8fe66e3557f5fa9b071b627ac3f9c81b3976c6
Nicolas Bonnet <mail@nicolasbon.net>
2025-07-19 12:37:16 +0200

    Add protobuf structures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod generated;
+
+use tonic::transport::Channel;
+
+
+
+tonic::include_proto!("envoy.service.ext_proc.v3");
+
+type Result<T, E=Box<dyn std::error::Error>> = std::result::Result<T, E>;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn call_ext_proc(channel: Channel) -> Result<()> {
+    let mut client = external_processor_client::ExternalProcessorClient::new(channel);
+    Ok(())
 }
