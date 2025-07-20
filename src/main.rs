8641d116019f9cdd8a7a83ff5ae0c09f25150a67
Nicolas Bonnet <mail@nicolasbon.net>
2025-07-20 12:25:48 +0200

    Use indicatif for progress bars

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod generated;
 
-use std::{env, path::{Path, PathBuf}, time::Duration};
+use std::{
+    env, io::Write, path::{Path, PathBuf}, time::Duration
+};
 
 use crate::generated::envoy::{
     config::core::v3::{HeaderMap, HeaderValue},
@@ -11,15 +13,21 @@ use crate::generated::envoy::{
 };
 use clap::Parser;
 use futures::stream::FuturesUnordered;
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use thiserror::Error;
-use tokio::{fs::File, io::{AsyncWriteExt, BufWriter}, time::Instant};
+use tokio::{
+    fs::File,
+    io::{AsyncWriteExt, BufWriter},
+    time::Instant,
+};
 use tokio::{
     select,
     sync::mpsc::{self, error::SendError},
 };
 use tokio_stream::{StreamExt as _, wrappers::ReceiverStream};
 use tonic::transport::Channel;
-use tracing::{debug, info, trace};
+use zstd::Encoder;
+// use tracing::{debug, info, trace};
 
 type Result<T, E = Error> = std::result::Result<T, E>;
 
@@ -55,66 +63,96 @@ struct Cli {
     result_directory: Option<PathBuf>,
 }
 
-
 fn validate_test_duration_seconds(v: &str) -> Result<Duration, String> {
-    let v: u64 = v.parse().map_err(|_| format!("test duration must be a number, got {}", v))?;
+    let v: u64 = v
+        .parse()
+        .map_err(|_| format!("test duration must be a number, got {}", v))?;
     if v <= 0 {
-        return Err(format!("test duration must be strictly positive, got {}", v));
+        return Err(format!(
+            "test duration must be strictly positive, got {}",
+            v
+        ));
     }
 
     Ok(Duration::from_secs(v))
 }
 
 fn validate_start_throughput(v: &str) -> Result<f32, String> {
-    let v: f32 = v.parse().map_err(|_| format!("start throughput must be a number, got {}", v))?;
-    if !v.is_finite() || v <= 0. {
+    let v: f32 = v
+        .parse()
+        .map_err(|_| format!("start throughput must be a number, got {}", v))?;
+    if !v.is_finite() {
         return Err(format!("start throughput must be finite, got {}", v));
     }
 
     if v <= 0. {
-        return Err(format!("start throughput must be strictly positive, got {}", v));
+        return Err(format!(
+            "start throughput must be strictly positive, got {}",
+            v
+        ));
     }
 
     Ok(v)
 }
 
 fn validate_end_throughput(v: &str) -> Result<f32, String> {
-    let v: f32 = v.parse().map_err(|_| format!("end throughput must be a number, got {}", v))?;
-    if !v.is_finite() || v <= 0. {
+    let v: f32 = v
+        .parse()
+        .map_err(|_| format!("end throughput must be a number, got {}", v))?;
+    if !v.is_finite() {
         return Err(format!("end throughput must be finite, got {}", v));
+    }
+
+    if v <= 0. {
+        return Err(format!(
+            "end throughput must be strictly positive, got {}",
+            v
+        ));
     }
 
     Ok(v)
 }
 
 fn validate_throughput_step(v: &str) -> Result<f32, String> {
-    let v: f32 = v.parse().map_err(|_| format!("throughput step must be a number, got {}", v))?;
-    if !v.is_finite() || v <= 0. {
+    let v: f32 = v
+        .parse()
+        .map_err(|_| format!("throughput step must be a number, got {}", v))?;
+    if !v.is_finite() {
         return Err(format!("throughput step must be finite, got {}", v));
     }
 
     if v < 0. {
-        return Err(format!("throughput step must be above or equal to 0, got {}", v));
+        return Err(format!(
+            "throughput step must be above or equal to 0, got {}",
+            v
+        ));
     }
 
     Ok(v)
 }
 
 fn validate_throughput_multiplier(v: &str) -> Result<f32, String> {
-    let v: f32 = v.parse().map_err(|_| format!("throughput multiplier must be a number, got {}", v))?;
-    if !v.is_finite() || v <= 0. {
+    let v: f32 = v
+        .parse()
+        .map_err(|_| format!("throughput multiplier must be a number, got {}", v))?;
+    if !v.is_finite() {
         return Err(format!("throughput multiplier must be finite, got {}", v));
     }
 
     if v < 1. {
-        return Err(format!("throughput multiplier must be above or equal to 1, got {}", v));
+        return Err(format!(
+            "throughput multiplier must be above or equal to 1, got {}",
+            v
+        ));
     }
 
     Ok(v)
 }
 
 fn validate_result_directory(v: &str) -> Result<PathBuf, String> {
-    let v: PathBuf = v.parse().map_err(|_| format!("result directory must be a path, got {}", v))?;
+    let v: PathBuf = v
+        .parse()
+        .map_err(|_| format!("result directory must be a path, got {}", v))?;
 
     if !v.is_dir() {
         return Err(format!("result directory is not a directory"));
@@ -127,7 +165,7 @@ fn validate_result_directory(v: &str) -> Result<PathBuf, String> {
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     let cli = Cli::parse();
-    trace!(cli=?cli, "parsed cli");
+    // trace!(cli=?cli, "parsed cli");
 
     let channel = tonic::transport::Endpoint::new(cli.uri.clone())
         .map_err(Error::FailedToCreateEndpoint)?
@@ -141,20 +179,20 @@ async fn main() -> Result<()> {
         Ok(start.elapsed())
     };
 
-    let result_directory = match &cli.result_directory{
+    let result_directory = match &cli.result_directory {
         Some(dir) => Path::new(dir),
         None => &env::current_dir().expect("current directory can be read"),
     };
 
-    info!(
-        test_duration=?cli.test_duration, 
-        start_throughput=cli.start_throughput,
-        end_throughput=cli.end_throughput,
-        throughput_multiplier=cli.throughput_multiplier,
-        throughput_step=cli.throughput_step,
-        result_directory=?result_directory,
-        "starting load test"
-    );
+    // info!(
+    //     test_duration=?cli.test_duration,
+    //     start_throughput=cli.start_throughput,
+    //     end_throughput=cli.end_throughput,
+    //     throughput_multiplier=cli.throughput_multiplier,
+    //     throughput_step=cli.throughput_step,
+    //     result_directory=?result_directory,
+    //     "starting load test"
+    // );
 
     run(&cli, &worker, result_directory).await
 }
@@ -163,36 +201,71 @@ async fn run<Fut>(cli: &Cli, run_worker: &impl Fn() -> Fut, result_directory: &P
 where
     Fut: Future<Output = Result<Duration>>,
 {
+    let throughputs = get_all_throughputs(cli);
+    let multi_progress = MultiProgress::new();
+    let progress_style = ProgressStyle::with_template(
+        "[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}",
+    )
+    .unwrap()
+    .progress_chars("##-");
 
-    let mut i = 0;
-    while get_throughput(cli, i) <= cli.end_throughput {
-        let throughput = get_throughput(cli, i);
-        let deadline = tokio::time::Instant::now() + cli.test_duration;
+    let mut progress_bars = vec![];
+    for throughput in &throughputs {
+        let estimated_request_count = cli.test_duration.as_secs_f32() * (*throughput as f32);
 
-        run_with_throughput(cli, throughput, deadline, run_worker, result_directory).await?;
-        i += 1;
+        let pb = multi_progress.add(ProgressBar::new(estimated_request_count as u64));
+        pb.set_style(progress_style.clone());
+        pb.set_message(format!("{} req/s", throughput));
+        progress_bars.push(pb);
     }
 
-    info!(result_directory=?result_directory, "load test finished");
+    for (throughput, mut pb) in throughputs.into_iter().zip(progress_bars.into_iter()) {
+        let deadline = tokio::time::Instant::now() + cli.test_duration;
+        run_with_throughput(
+            &mut pb,
+            cli,
+            throughput,
+            deadline,
+            run_worker,
+            result_directory,
+        )
+        .await?;
+        pb.finish();
+    }
+
+    // info!(result_directory=?result_directory, "load test finished");
 
     Ok(())
 }
 
-fn get_throughput(cli: &Cli, i: u32) -> f32 {
-    let from: f32 = cli.start_throughput as f32;
+fn get_all_throughputs(cli: &Cli) -> Vec<f32> {
+    let from: f32 = cli.start_throughput;
     let step: f32 = cli.throughput_step;
     let mul: f32 = cli.throughput_multiplier;
-    let i: f32 = i as f32;
 
-    if mul == 1. {
-        return from + step * i;
+    let mut throughputs = vec![];
+    let mut i = 0_usize;
+    loop {
+        let throughput = if mul == 1. {
+            from + step * (i as f32)
+        } else {
+            let r = step / (1. - mul);
+            mul.powf(i as f32) * (from - r) + r
+        };
+
+        if throughput > cli.end_throughput {
+            break;
+        }
+
+        throughputs.push(throughput);
+        i += 1;
     }
 
-    let r = step / (1.-mul);
-    mul.powf(i) * (from-r) + r
+    throughputs
 }
 
 async fn run_with_throughput<Fut>(
+    pb: &mut ProgressBar,
     cli: &Cli,
     target_throughput: f32,
     deadline: tokio::time::Instant,
@@ -202,7 +275,7 @@ async fn run_with_throughput<Fut>(
 where
     Fut: Future<Output = Result<Duration>>,
 {
-    trace!(target_throughput, "starting test");
+    // trace!(target_throughput, "starting test");
 
     let mut timer = tokio::time::interval(Duration::from_secs_f32(1. / target_throughput));
     let mut f: FuturesUnordered<Fut> = FuturesUnordered::new();
@@ -215,25 +288,27 @@ where
         select! {
             _ = timer.tick() => {
                 f.push(run_worker());
+                pb.inc(1);
                 requests_sent += 1;
             }
 
             result = f.next() => {
-                trace!(result=?result, "worker finished");
+                // trace!(result=?result, "worker finished");
 
                 match result {
                     Some(result) => {
                         durations.push(result?);
                     }
                     None => {
-                    trace!("no ongoing workers, waiting for next interval");
+                    // trace!("no ongoing workers, waiting for next interval");
                     select! {
                         _ = timer.tick() => {
                             f.push(run_worker());
+                            pb.inc(1);
                             requests_sent += 1;
                         }
                         _ = tokio::time::sleep_until(deadline) => {
-                            trace!("deadline reached, no ongoing workers, quitting");
+                            // trace!("deadline reached, no ongoing workers, quitting");
                             break;
                         }
                     }
@@ -242,11 +317,11 @@ where
             }
 
             _ = tokio::time::sleep_until(deadline) => {
-                trace!("deadline reached, waiting for workers to finish");
-                while let Some(result) = f.next().await {
-                    trace!(result=?result, "worker finished");
+                // trace!("deadline reached, waiting for workers to finish");
+                while let Some(_) = f.next().await {
+                    // trace!(result=?result, "worker finished");
                 }
-                trace!("all workers finished");
+                // trace!("all workers finished");
                 break;
             }
         }
@@ -262,52 +337,71 @@ where
         ));
     }
 
-    write_report(result_directory, target_throughput, &durations).await.map_err(Error::WriteReportError)?;
+    write_report(result_directory, target_throughput, &durations)
+        .await
+        .map_err(Error::WriteReportError)?;
 
     let avg_duration = durations.iter().sum::<Duration>() / durations.len() as u32;
     let min_duration = durations.iter().min().unwrap();
     let max_duration = durations.iter().max().unwrap();
 
-    debug!(
+    pb.finish_with_message(format!(
+        "{} req/s: {} requests, avg: {:?}, min: {:?}, max: {:?}",
         target_throughput,
-        actual_throughput,
-        percent_of_target_throughput,
-        request_finished=durations.len(),
-        avg_duration=?avg_duration,
-        min_duration=?min_duration,
-        max_duration=?max_duration, 
-        "test finished");
+        durations.len(),
+        avg_duration,
+        min_duration,
+        max_duration,
+    ));
+
+    // debug!(
+    //     target_throughput,
+    //     actual_throughput,
+    //     percent_of_target_throughput,
+    //     request_finished=durations.len(),
+    //     avg_duration=?avg_duration,
+    //     min_duration=?min_duration,
+    //     max_duration=?max_duration,
+    //     "test finished");
 
     Ok(())
 }
 
-async fn write_report(directory_path: &Path, target_throughput: f32, durations: &[Duration]) -> Result<(), std::io::Error> {
-    let file_name = format!("durations_{}.json", target_throughput.floor() as u32 );
+async fn write_report(
+    directory_path: &Path,
+    target_throughput: f32,
+    durations: &[Duration],
+) -> Result<(), std::io::Error> {
+    let file_name = format!("durations_{}.json.txt.zst", target_throughput.floor() as u32);
     let file_path = directory_path.join(file_name);
 
-    debug!(file_path=?file_path, "writing report");
-    let f = File::create(file_path).await?;
-    let mut writer = BufWriter::new(f);
-    writer.write("[".as_bytes()).await?;
+    // let mut f = File::create(file_path).await?;
+    let mut v = Vec::new();
+    let mut encoder = Encoder::new(&mut v, 0)?;
+
+    encoder.write("[".as_bytes())?;
+
     let mut has_previous_value = false;
     for duration in durations {
         if has_previous_value {
-            writer.write(",".as_bytes()).await?;
+            encoder.write(",".as_bytes())?;
         }
-
-        writer.write(format!("{}", duration.as_nanos()).as_bytes()).await?;
+        encoder.write(format!("{}", duration.as_nanos()).as_bytes())?;
         has_previous_value = true;
     }
-    writer.write("]".as_bytes()).await?;
 
-    writer.flush().await?;
+    encoder.write("]".as_bytes())?;
+    encoder.finish()?;
+
+    let mut f = File::create(file_path).await?;
+    f.write_all(&v).await?;
+
     Ok(())
-
 }
 
 async fn call_ext_proc(channel: Channel) -> Result<()> {
     let mut client = ExternalProcessorClient::new(channel);
-    trace!("created client");
+    // trace!("created client");
 
     let (tx, rx) = mpsc::channel(2);
     tx.send(request_headers::create_processing_request())
@@ -315,35 +409,35 @@ async fn call_ext_proc(channel: Channel) -> Result<()> {
         .map_err(Error::CannotSendInitialRequest)?;
 
     let request_stream = ReceiverStream::new(rx);
-    trace!("created request stream");
+    // trace!("created request stream");
 
     let response = client
         .process(request_stream)
         .await
         .map_err(Error::FailedToCallExtProc)?;
-    trace!("received response");
+    // trace!("received response");
 
     let mut response_stream = response.into_inner();
 
-    let Some(processing_response) = response_stream.next().await else {
+    let Some(_processing_response) = response_stream.next().await else {
         // Early return if the stream is closed.
-        trace!("stream is closed");
+        // trace!("stream is closed");
         return Ok(());
     };
 
-    trace!(processing_response=?processing_response, "first processing_response received");
+    // trace!(processing_response=?processing_response, "first processing_response received");
 
     let Ok(_) = tx.send(response_headers::create_processing_request()).await else {
-        trace!("cannot send second request, stream is closed");
+        // trace!("cannot send second request, stream is closed");
         return Ok(());
     };
 
-    let Some(processing_response) = response_stream.next().await else {
-        trace!("cannot receive second response, stream is closed");
+    let Some(_processing_response) = response_stream.next().await else {
+        // trace!("cannot receive second response, stream is closed");
         return Ok(());
     };
 
-    trace!(processing_response=?processing_response, "second processing_response received");
+    // trace!(processing_response=?processing_response, "second processing_response received");
     Ok(())
 }
 
