c0005e61c4dae7553da3494c005f90e2116c56fb
Nicolas Bonnet <mail@nicolasbon.net>
2025-07-20 14:04:40 +0200

    Fix cargo fmt

--- build.rs
+++ build.rs
@@ -2,16 +2,16 @@ use std::io::Result;
 
 fn main() -> Result<()> {
     tonic_build::configure()
-    .build_client(true)
-    .build_server(false)
-    .compile_protos(
-        &["proto/envoy/api/envoy/service/ext_proc/v3/external_processor.proto"],
-        &[
-            "proto/envoy/api",
-            "proto/protoc-gen-validate",
-            "proto/udpa",
-            "proto/xds",
-        ],
-    )?;
+        .build_client(true)
+        .build_server(false)
+        .compile_protos(
+            &["proto/envoy/api/envoy/service/ext_proc/v3/external_processor.proto"],
+            &[
+                "proto/envoy/api",
+                "proto/protoc-gen-validate",
+                "proto/udpa",
+                "proto/xds",
+            ],
+        )?;
     Ok(())
 }
