3c0bce9cc12fd2e27817f42668413d80c67c84de
Nicolas Bonnet <mail@nicolasbon.net>
2025-07-20 13:45:06 +0200

    Fix visibility

--- src/generated.rs
+++ src/generated.rs
@@ -1,25 +1,28 @@
-pub mod envoy {
-    pub mod service {
-        pub mod ext_proc {
-            pub mod v3 {
+pub(crate) mod envoy {
+    pub(crate) mod service {
+        pub(crate) mod ext_proc {
+            pub(crate) mod v3 {
+                #![allow(clippy::all)]
                 tonic::include_proto!("envoy.service.ext_proc.v3");
             }
         }
     }
 
-    pub mod config {
-        pub mod core {
-            pub mod v3 {
+    pub(crate) mod config {
+        pub(crate) mod core {
+            pub(crate) mod v3 {
+                #![allow(clippy::all)]
                 tonic::include_proto!("envoy.config.core.v3");
             }
         }
     }
 
-    pub mod extensions {
-        pub mod filters {
-            pub mod http {
-                pub mod ext_proc {
-                    pub mod v3 {
+    pub(crate) mod extensions {
+        pub(crate) mod filters {
+            pub(crate) mod http {
+                pub(crate) mod ext_proc {
+                    pub(crate) mod v3 {
+                        #![allow(clippy::all)]
                         tonic::include_proto!("envoy.extensions.filters.http.ext_proc.v3");
                     }
                 }
@@ -27,16 +30,18 @@ pub mod envoy {
         }
     }
 
-    pub mod r#type {
-        pub mod v3 {
+    pub(crate) mod r#type {
+        pub(crate) mod v3 {
+            #![allow(clippy::all)]
             tonic::include_proto!("envoy.r#type.v3");
         }
     }
 }
 
-pub mod xds {
-    pub mod core {
-        pub mod v3 {
+pub(crate) mod xds {
+    pub(crate) mod core {
+        pub(crate) mod v3 {
+            #![allow(clippy::all)]
             tonic::include_proto!("xds.core.v3");
         }
     }
