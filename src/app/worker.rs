a771cd2fb1a3a53a93cb9bc8433bc93a964f8ce5
Nicolas Bonnet <mail@nicolasbon.net>
2025-08-03 18:37:30 +0200

    Use scheduler

--- src/app/worker.rs
+++ src/app/worker.rs
@@ -1,6 +1,66 @@
-use crate::app::error::Result;
+use tokio::sync::mpsc;
+use tokio_stream::{StreamExt, wrappers::ReceiverStream};
+use tonic::transport::Channel;
+
+use crate::{
+    app::{
+        error::{Error, Result},
+        sample_requests::{request_headers, response_headers},
+    },
+    generated::envoy::service::ext_proc::v3::external_processor_client::ExternalProcessorClient,
+};
 
 #[allow(dead_code)]
 pub(crate) trait Worker {
     fn run(&self) -> impl Future<Output = Result<()>> + Send;
+}
+
+#[derive(Debug, Clone)]
+#[allow(dead_code)]
+pub(crate) struct GrpcWorker {
+    channel: Channel,
+}
+
+impl GrpcWorker {
+    #[allow(dead_code)]
+    pub(crate) fn new(channel: &Channel) -> Self {
+        Self {
+            channel: channel.clone(),
+        }
+    }
+}
+
+impl Worker for GrpcWorker {
+    async fn run(&self) -> Result<()> {
+        let mut client = ExternalProcessorClient::new(self.channel.clone());
+
+        let (tx, rx) = mpsc::channel(2);
+        tx.send(request_headers::create_processing_request())
+            .await
+            .map_err(|e| Error::CannotSendInitialRequest(Box::new(e)))?;
+
+        let request_stream = ReceiverStream::new(rx);
+
+        let response = client
+            .process(request_stream)
+            .await
+            .map_err(|e| Error::FailedToCallExtProc(Box::new(e)))?;
+
+        let mut response_stream = response.into_inner();
+
+        let Some(_processing_response) = response_stream.next().await else {
+            // Early return if the stream is closed.
+            return Ok(());
+        };
+
+        let Ok(()) = tx.send(response_headers::create_processing_request()).await else {
+            return Ok(());
+        };
+
+        let Some(_processing_response) = response_stream.next().await else {
+            return Ok(());
+        };
+
+        Ok(())
+    }
 }
